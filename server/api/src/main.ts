d76840c525c00447e3f12d8eb81b2e271d4e018a
황예원 <cakejuuu@gmail.com>
2025-05-02 10:55:09 +0900

    init: Swagger 설정 추가
    - Swagger 설정 파일 config에 추가
    - JWT 인증 스킴 사전 등록 (주석 처리 상태로 추후 JWT 인증 구현 완료 시 주석 해제 필요)
    
    jira ref: #S12P31S303-49

--- server/api/src/main.ts
+++ server/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './common/config/swagger'; // ← 경로 맞게 추가
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
